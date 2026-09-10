5db6efc047fb0c1c91e1bd58f7ee9ebd5213c0d5
vigaz <pudimdebacon3@gmail.com>
2026-09-09 23:03:46 -0300

    Add sepolicy fixes ported from AlphaDroid device tree

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -142,6 +142,7 @@ TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 # SEPolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
